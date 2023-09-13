fed7af64d0030d9efba3508ae970e8a95d28ce31
David Butrón <dabicho803@gmail.com>
2023-09-12 19:45:38 -0600

    feat: Add app splash and login

--- app/src/main/java/com/davedevab/spotifyuiclone/MainActivity.kt
+++ app/src/main/java/com/davedevab/spotifyuiclone/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.davedevab.spotifyuiclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.davedevab.spotifyuiclone.login.LogIn
 
 class MainActivity : AppCompatActivity() {
+    private val looper = Looper.myLooper()
+    private val handler = looper?.let { Handler(it) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        handler?.postDelayed({
+            val intent = Intent(this@MainActivity, LogIn::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+
     }
 }
---
+++ app/src/main/java/com/davedevab/spotifyuiclone/login/LogIn.kt
@@ -0,0 +1,12 @@
+package com.davedevab.spotifyuiclone.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.davedevab.spotifyuiclone.R
+
+class LogIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
