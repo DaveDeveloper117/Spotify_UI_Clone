a52fb6421b1bcb19e8f0332b5986a158d7d18d2b
David Butrón <dabicho803@gmail.com>
2023-09-16 14:24:03 -0600

    feat: Add mc app center

--- app/src/main/java/com/davedevab/spotifyuiclone/MainActivity.kt
+++ app/src/main/java/com/davedevab/spotifyuiclone/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.davedevab.spotifyuiclone
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import com.davedevab.spotifyuiclone.databinding.ActivityLogInBinding
+import androidx.appcompat.app.AppCompatActivity
 import com.davedevab.spotifyuiclone.databinding.ActivityMainBinding
 import com.davedevab.spotifyuiclone.login.LogIn
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
     private  lateinit var binding: ActivityMainBinding
@@ -17,6 +19,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        AppCenter.start(
+            application, "{7074c186-45bb-49fb-92c1-a1ce9639f8bb}",
+            Analytics::class.java, Crashes::class.java
+        )
         handler?.postDelayed({
             intent = Intent(this@MainActivity, LogIn::class.java)
             startActivity(intent)
