4ba5823773d9a4894d7cdba7f4df408405760a41
David Butrón <dabicho803@gmail.com>
2023-09-16 14:34:18 -0600

    fix: remove mc app center

--- app/src/main/java/com/davedevab/spotifyuiclone/MainActivity.kt
+++ app/src/main/java/com/davedevab/spotifyuiclone/MainActivity.kt
@@ -7,9 +7,7 @@ import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.davedevab.spotifyuiclone.databinding.ActivityMainBinding
 import com.davedevab.spotifyuiclone.login.LogIn
-import com.microsoft.appcenter.AppCenter
-import com.microsoft.appcenter.analytics.Analytics
-import com.microsoft.appcenter.crashes.Crashes
+
 
 class MainActivity : AppCompatActivity() {
     private  lateinit var binding: ActivityMainBinding
@@ -19,10 +17,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        AppCenter.start(
-            application, "{7074c186-45bb-49fb-92c1-a1ce9639f8bb}",
-            Analytics::class.java, Crashes::class.java
-        )
         handler?.postDelayed({
             intent = Intent(this@MainActivity, LogIn::class.java)
             startActivity(intent)
